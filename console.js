1412456942001c64a6f4ad8826ea953cdabf7b04
Claudia T <timeisyummy@gmail.com>
2015-06-13 01:53:52 +1000

    Working input to console

--- console.js
+++ console.js
@@ -26,15 +26,24 @@ function printBoard(boardstate) {
 
 function askWhite() {
   process.stdin.resume();
-  process.stdout.write("White's turn - enter input > ");
-  process.stdout.write("E.g. '1,4 2,4'")
+  process.stdout.write("White's turn - enter input. For example:\n");
+  process.stdout.write("'1,4 2,4' - Only the position of the digits matter.\n")
+  process.stdout.write("> ")
   process.stdin.once('data', function(data) {
     data = data.toString().trim();
-    var src_x = 0;
-    var src_y = 0;
-//    if (board.isValidMove(chess.WHITE, new chess.Pos(x, y))) {
- //     askWhite();
-  //  }
+    var src_x = data[0];
+    var src_y = data[2];
+    var src = new chess.Pos(src_x, src_y);
+    var dest_x = data[4];
+    var dest_y = data[6];
+    var dest = new chess.Pos(dest_x, dest_y);
+    var move = new chess.Move(src, dest);
+
+    console.log('src ' + src_x + ',' + src_y +
+      ' dest ' + dest_x + ',' + dest_y)
+    if (board.isValidMove(chess.WHITE, move)) {
+      askWhite();
+    }
     askBlack(move);
   });
 }
@@ -44,11 +53,16 @@ function askBlack(move) {
   process.stdout.write("Black's turn - enter input > ");
   process.stdin.once('data', function(data) {
     data = data.toString().trim();
-    var src_x = 0;
-    var src_y = 0;
-//    if (board.isValidMove(chess.BLACK, new chess.Pos(x,y))) {
-//      askBlack();
-//    }
+    var src_x = data[0];
+    var src_y = data[2];
+    var src = new chess.Pos(src_x, src_y);
+    var dest_x = data[4];
+    var dest_y = data[6];
+    var dest = new chess.Pos(dest_x, dest_y);
+    var move = new chess.Move(src, dest);
+    if (board.isValidMove(chess.BLACK, move)) {
+      askBlack();
+    }
     // make move
     board.makeMove();
     printBoard(board.getBoard());
