e7f7c70fa8d890658439799a9e8cea47b6fb3eb2
Leo Huang <crazysheep2302@gmail.com>
2015-06-14 01:31:51 +1000

    Add stubs for chess-canvas.js and add browserify-middleware to auto compile

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var chess = require('./routes/chess');
 
 var app = express();
 
@@ -20,13 +21,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(path.join(__dirname, 'public/js',
-        require('browserify-middleware')(path.join(__dirname, 'client'))));
+app.use('/js', require('browserify-middleware')(path.join(__dirname, 'client')));
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/chess', chess);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ client/chess-canvas.js
@@ -0,0 +1,1 @@
+var chess = require('../common/chess');
