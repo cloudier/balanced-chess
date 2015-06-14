363df8ea6ca2ccd09cac70e6a058f5d68bb1281a
Leo Huang <crazysheep2302@gmail.com>
2015-06-14 19:02:21 +1000

    Add client code to detect click event positions and refactor to use 2 spaces

--- client/chess-canvas.js
+++ client/chess-canvas.js
@@ -1,12 +1,20 @@
 var chess = require('../common/chess');
 
+/*
+ * Extends chess.Board() and adds methods for drawing the board
+ * to an html canvas object as well as methods to interact with
+ * the board from the browser.
+ */
 function DrawableBoard(id) {
 	chess.Board.call(this);
 	var canvas = document.getElementById(id);
+	var ctx = canvas.getContext('2d');
+	var cellSize = canvas.width / chess.BOARD_SIZE;
 
+  /*
+   * Draw the current board state to the canvas
+   */
 	this.draw = function() {
-		var ctx = canvas.getContext('2d');
-		var cellSize = canvas.width / chess.BOARD_SIZE;
 		var board = this.getBoard();
 
 		var fontSize = Math.floor(cellSize * 0.7);
@@ -26,13 +34,48 @@ function DrawableBoard(id) {
 				} else {
 					ctx.fillText(text, x * cellSize + offset, (y+1) * cellSize - offset, cellSize);
 				}
-				
 			}
 		}
+	};
+
+  /*
+   * Handles a click event that occured on the canvas
+   */
+	this.click = function(event) {
+		var clickPos = canvas.relMouseCoords(event);
+
+		var cellPos = new chess.Pos(Math.floor(clickPos.x / cellSize),
+									Math.floor(clickPos.y / cellSize));
+
+		console.log(cellPos);
 	};
 }
 DrawableBoard.prototype = Object.create(chess.Board.prototype);
 DrawableBoard.prototype.constructor = DrawableBoard;
+
+/*
+ * Given a mouse event, get the mouse coords relative to the
+ * canvas object.
+ *
+ * Returns an {x: _, y: _} object.
+ */
+HTMLCanvasElement.prototype.relMouseCoords = function (event) {
+  var totalOffsetX = 0;
+  var totalOffsetY = 0;
+  var canvasX = 0;
+  var canvasY = 0;
+  var currentElement = this;
+
+  do {
+    totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+    totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+  } while (currentElement = currentElement.offsetParent);
+
+  canvasX = event.pageX - totalOffsetX;
+  canvasY = event.pageY - totalOffsetY;
+
+  return {x: canvasX, y: canvasY};
+};
 
 
 /*
